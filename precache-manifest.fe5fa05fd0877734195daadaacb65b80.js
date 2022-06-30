869cd3bfdd68fd1dc23117e68d32c0d91b11c016
maithili17 <maithilisingh1711@gmail.com>
2022-06-30 11:55:02 +0530

    Updates

---
+++ precache-manifest.fe5fa05fd0877734195daadaacb65b80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a9ba9abea0f3fb0c683987a4fa0efe",
+    "url": "/react-dashboard/index.html"
+  },
+  {
+    "revision": "434ad9581660ba2da20e",
+    "url": "/react-dashboard/static/css/main.fcb9850c.chunk.css"
+  },
+  {
+    "revision": "44bd5997ff04fc1f7918",
+    "url": "/react-dashboard/static/js/2.4047a999.chunk.js"
+  },
+  {
+    "revision": "67ed965539c138338309bad4215002b8",
+    "url": "/react-dashboard/static/js/2.4047a999.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "434ad9581660ba2da20e",
+    "url": "/react-dashboard/static/js/main.086eaeb4.chunk.js"
+  },
+  {
+    "revision": "7da2fb1e33e436b37e3d",
+    "url": "/react-dashboard/static/js/runtime-main.f659432f.js"
+  }
+]);
